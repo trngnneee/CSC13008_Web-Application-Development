1d95804e56dd7eb66f5549bab12ecded721ef88b
khoavadienq <ddkhoa9@gmail.com>
2025-10-13 01:58:10 +0700

    Done hw1

--- homework/index.js
+++ homework/index.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'about.html'));
 });
 
 app.listen(port, () => {
