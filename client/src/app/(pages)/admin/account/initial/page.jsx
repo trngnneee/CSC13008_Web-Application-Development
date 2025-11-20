c98ed6c5b5389d054e9b22911dd72fdca5189bd9
trngnneee <dtn06052005@gmail.com>
2025-11-20 11:01:12 +0700

    Verify email sau khi đăng ký với FE

--- client/src/app/(pages)/admin/account/initial/page.jsx
+++ client/src/app/(pages)/admin/account/initial/page.jsx
@@ -2,7 +2,7 @@ export default function InitialPage(){
   return (
     <>
       <div className="font-bold text-[25px] text-[var(--main-color)] text-center">Tài khoản chưa được kích hoạt</div>
-      <div className="text-gray-400 mb-10 text-center">Vui lòng chờ cho Admin duyệt tài khoản của bạn</div>
+      <div className="text-gray-400 mb-10 text-center">Vui lòng kiểm tra email của bạn để kích hoạt tài khoản</div>
     </>
   )
 }
